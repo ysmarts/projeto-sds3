5d2a10eebf06cf8f811beafbb0c594466f1dfe22
Ysmar <ysmarvictor@gmail.com>
2021-11-07 03:39:24 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service //Irá registrar a classe como um componente
+public class SellerService {
+
+	@Autowired //Irá injetar a dependência de forma transparente -> ele instancia automaticamente (new)
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList()); //Expressão Lambda para converter o Seller em SellerDTO
+	}
+}
