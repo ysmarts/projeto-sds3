260f42aaa9da49064073a08a6f5c24788abfea2d
Ysmar <ysmarvictor@gmail.com>
2021-11-07 21:17:30 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+//?? -> Se a variável de ambiente não estiver definida no ambiente que está rodando, ele irá pegar o valor a direita
